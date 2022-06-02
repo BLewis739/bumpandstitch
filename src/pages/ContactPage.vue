989146b081a9418ce587fd5f22025c38bd0609b2
Brad Lewis <BLewis739@gmail.com>
2022-06-02 13:38:45 -0400

    iframe for form added, needs styling

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -6,7 +6,7 @@
     <div class='contact-description'>
     </div>
     <div class='contact-form-wrapper'>
-      Coming soon...
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfuRim1fnNhRFQ192-hKVHbnxLYPiB9euS353_PHrjT2LnInQ/viewform?embedded=true" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
     </div>
   </div>
 </template>
