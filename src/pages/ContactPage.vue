59d905b734065aacf5e4e236d7cae3d0d2f210ca
Brad Lewis <BLewis739@gmail.com>
2022-06-02 14:37:24 -0400

    Most contact on contact page

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -4,7 +4,28 @@
       Contact Us
     </header>
     <div class='contact-description'>
-      
+      <div class='contact-intro'>
+        Please feel free to reach out to us through social media! We are on both Twitter and Instagram, and 
+        aim to post regular updates about new projects, gear designs, and in-ring shots from wrestlers who 
+        have chosen to work with us. Give us a follow on social media to stay updated!
+      </div>
+      <div class="social-row">
+      <a href="https://www.instagram.com/bumpandstitch" target="_blank"><img src="../icons/Insta.jpg" class='icon'></a>
+      <div class="handle">
+        @BumpAndStitch
+      </div>
+      </div>
+      <div class='social-row'>
+      <a href="https://twitter.com/bumpandstitch" target="_blank"><img src="../icons/TwitterLogo.png" class='icon'></a>
+      <div class="handle">
+        @BumpAndStitch
+      </div>
+      </div>
+      <div class='contact-intro'>
+        If you are interesting in working with us for your next set of gear, please complete the Google Form 
+        below. A representative will contact you within 3-5 business days to confirm your inquiry and begin 
+        discussing the specifics of your project.
+      </div>
     </div>
     <div class='contact-form-wrapper'>
       <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfuRim1fnNhRFQ192-hKVHbnxLYPiB9euS353_PHrjT2LnInQ/viewform?embedded=true" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
