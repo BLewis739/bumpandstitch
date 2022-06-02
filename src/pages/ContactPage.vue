cb64ea7a107a370654445dc23ec5f1e3f1e2d4ec
Brad Lewis <BLewis739@gmail.com>
2022-06-02 13:50:49 -0400

    Styling updates

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -4,6 +4,7 @@
       Contact Us
     </header>
     <div class='contact-description'>
+      
     </div>
     <div class='contact-form-wrapper'>
       <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfuRim1fnNhRFQ192-hKVHbnxLYPiB9euS353_PHrjT2LnInQ/viewform?embedded=true" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
