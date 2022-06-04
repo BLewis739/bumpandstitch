1101f5febce539286445c6e2eb202b3ff53b32a7
Brad Lewis <BLewis739@gmail.com>
2022-06-04 16:16:53 -0400

    About page text changes

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -4,9 +4,10 @@
       About
     </header>
       <div class="about-text">
-      Bump and Stitch was officially founded in April 2022, but was built from years of word-of-mouth reputation 
-      for high-quality work. Over 3 years of making gear, our founder has constructed upwards of 100 items, ranging 
-      from full designs to alterations of already-finished gear. We aspire to improve and grow with every project, 
+      Bump and Stitch was officially founded in April 2022, but was built from years of individual requests and projects. 
+      Over 3 years of making gear, our founder has constructed upwards of 100 items, ranging from full designs to alterations 
+      of already-finished gear. The official Bump and Stitch business was launched due to the high number of requests 
+      being received after having built up a reputation of high-quality work. We aspire to improve and grow with every project, 
       and are happy to take on new challenges. 
       </div>
     <div class="contact-text">
